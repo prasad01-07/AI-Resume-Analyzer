fcc6af0412815ce6d98d98fe7b77768776507057
prasad <jprasadprasad64@gmail.com>
2026-06-06 20:05:34 +0530

    Fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -17,9 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-8xtirc4q$!wx4%%nufa6t4lu8n43l@zor6l9rwm*cwsb539j30'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+                 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
